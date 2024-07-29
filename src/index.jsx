080ef8a4e444103c6be0c7589f981500de2bd743
Bruno Guilherme Gonçalves de Oliveira <ra55498652895@cursos.insf>
2024-07-29 08:27:05 -0300

    init

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './pages/Inicio';
+import Sobre from "./pages/Sobre"
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <App/> }/>
+        <Route path="/sobre" element={ <Sobre/> }/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/pages/Inicio/index.jsx
@@ -0,0 +1,19 @@
+import './index.css';
+
+import {Link} from "react-router-dom"
+import 
+
+export default function App() {
+  return (
+    <div className='container'>
+      <div className='cabeçalho'>
+        </img src={}>
+      </div>
+      <div className='conteudo'>
+
+      </div>
+    </div>
+  );
+}
+
+
